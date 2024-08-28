e09f99fd4ca22c1acf0d59c95d1eec346e7e4a82
ganocci <oliveirajoaopedro01@gmail.com>
2024-08-28 16:34:08 -0300

    Spring Boot API-REST

---
+++ src/main/java/com/joao/apirestjgm/ApirestjgmApplication.java
@@ -0,0 +1,14 @@
+package com.joao.apirestjgm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApirestjgmApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(ApirestjgmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/joao/apirestjgm/ApirestjgmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joao.apirestjgm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApirestjgmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
