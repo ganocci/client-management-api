3bcfea0d4aac0414502d46fb8bf07a06d83e51e5
ganocci <oliveirajoaopedro01@gmail.com>
2024-09-01 14:33:55 -0300

    Projeto Finalizado

--- src/main/java/com/joao/apirestjgm/ApirestjgmApplication.java
+++ src/main/java/com/joao/apirestjgm/ApirestjgmApplication.java
@@ -1,9 +1,15 @@
 package com.joao.apirestjgm;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@OpenAPIDefinition(info = @Info(title = "API REST JGM", version = "1",
+		description = "API RESTful para cadastro de clientes."))
+@EnableScheduling
 public class ApirestjgmApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/joao/apirestjgm/controllers/ClienteController.java
+++ src/main/java/com/joao/apirestjgm/controllers/ClienteController.java
@@ -2,6 +2,10 @@ package com.joao.apirestjgm.controllers;
 
 import com.joao.apirestjgm.models.dtos.ClienteDTO;
 import com.joao.apirestjgm.services.ClienteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,37 +15,76 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/clientes")
+@RequestMapping(value = "/clientes", produces = ("application/json"))
+@Tag(name = "API REST JGM")
 public class ClienteController {
 
     @Autowired
     private ClienteService clienteService;
 
     @GetMapping
+    @Operation(summary = "Listar todos os clientes", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Clientes retornados com sucesso"),
+            @ApiResponse(responseCode = "204", description = "Nenhum cliente encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @PostMapping
     public ResponseEntity<List<ClienteDTO>> listarTodos() {
         List<ClienteDTO> clientes = clienteService.listarClientes();
         return ResponseEntity.ok(clientes);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Buscar cliente por ID",
+            description = "Recupera os detalhes de um cliente específico pelo ID.", method = "GET")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente retornado com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<ClienteDTO> buscarPorId(@PathVariable Long id) {
         ClienteDTO cliente = clienteService.buscarClientePorId(id);
         return ResponseEntity.ok(cliente);
     }
 
     @PostMapping
+    @Operation(summary = "Criar um novo cliente", method = "POST")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Cliente criado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados de entrada inválidos"),
+            @ApiResponse(responseCode = "409", description = "CPF já cadastrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @ResponseStatus(HttpStatus.CREATED)
     public ResponseEntity<ClienteDTO> criar( @Valid @RequestBody ClienteDTO clienteDTO) {
         ClienteDTO novoCliente = clienteService.criarCliente(clienteDTO);
         return new ResponseEntity<>(novoCliente, HttpStatus.CREATED);
     }
 
     @PutMapping("/{id}")
+    @Operation(summary = "Atualizar os dados de um cliente",
+            description = "Atualiza os dados de um cliente existente pelo ID.", method = "PUT")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Cliente atualizado com sucesso"),
+            @ApiResponse(responseCode = "400", description = "Dados de entrada inválidos"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
     public ResponseEntity<ClienteDTO> atualizar(@PathVariable Long id, @RequestBody ClienteDTO clienteDTO) {
         ClienteDTO clienteAtualizado = clienteService.atualizarCliente(id, clienteDTO);
         return ResponseEntity.ok(clienteAtualizado);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Excluir um cliente",
+            description = "Exclui um cliente específico pelo ID.", method = "DELETE")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "204", description = "Cliente excluído com sucesso"),
+            @ApiResponse(responseCode = "404", description = "Cliente não encontrado"),
+            @ApiResponse(responseCode = "500", description = "Erro interno do servidor")
+    })
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public ResponseEntity<Void> deletar(@PathVariable Long id) {
         clienteService.deletarCliente(id);
         return ResponseEntity.noContent().build();
